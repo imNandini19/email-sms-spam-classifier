79352a922699442c53d8ea17d240174434740095
imNandini19 <n210843@rguktn.ac.in>
2025-09-19 19:43:37 +0530

    files are updated for deployment

--- app.py
+++ app.py
@@ -4,61 +4,64 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
+
+# Safe check for required NLTK resources
+for resource in ["punkt", "punkt_tab", "stopwords"]:
+    try:
+        if resource == "stopwords":
+            nltk.data.find("corpora/stopwords")
+        else:
+            nltk.data.find(f"tokenizers/{resource}")
+    except LookupError:
+        nltk.download(resource)
 
 ps = PorterStemmer()
-def transform_text(text):
-      text=text.lower()
-      text=nltk.word_tokenize(text)
-      
-      y=[]
-      for i in text:
-            if i.isalnum():
-                  y.append(i)
-      text = y[:]
-      y.clear()
-      for i in text:
-            if i not in stopwords.words('english') and i not in string.punctuation:
-                  y.append(i)
-                  
-      text = y[:]
-      y.clear()
-      for i in text:
-            y.append(ps.stem(i))
-      
-      return " ".join(y)
 
-tfidf=pickle.load(open('vectorizer.pkl', 'rb'))
-model=pickle.load(open('model.pkl', 'rb'))
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+# Load vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS spam Classifier")
- 
-input_sms=st.text_area("Enter the message")
+
+input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-      # preprocess
-      transformed_sms=transform_text(input_sms)
+    # Preprocess
+    transformed_sms = transform_text(input_sms)
 
-      # vectorize
-      vector_input = tfidf.transform([transformed_sms])
+    # Vectorize
+    vector_input = tfidf.transform([transformed_sms])
 
-      # predict
-      result = model.predict(vector_input)[0]
+    # Predict
+    result = model.predict(vector_input)[0]
 
-      # display
-      if result==1:
-            st.header("Spam")
-      else:
-            st.header("Not spam")
-
-
-
-
-
-
-
-
-
-
-
+    # Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not spam")
